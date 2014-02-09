0fce7f20b8e97cd9dd4a9be4eaaea9750ba2e17d
zornstar <zornstar@gmail.com>
2014-02-09 15:23:56 -0500

    Update simulate.js

--- simulate.js
+++ simulate.js
@@ -1,7 +1,8 @@
 function simulatePPClient() {
     
-    var hickoryFarmsFile = new DiffFile("hickoryfarms.com", "Privacy Policy.txt", "41d6f3ea68cf871152b433b13069125284e48b36");
+    var hickoryFarmsFile = new DiffFile("hickoryfarms.com", "Privacy Policy.txt", "https://www.hickoryfarms.com/_hf-privacy-policy");
 
+    hickoryFarmsFile.sha = "41d6f3ea68cf871152b433b13069125284e48b36";
     PPClient.get_new_and_old_file(hickoryFarmsFile, function(file) {
        
         var diff = HtmlDiff.formatTextDiff(file.old, file.current);
