5a39fc50f71178437ab1c5c432aa6a47591d3760
zornstar <zornstar@gmail.com>
2014-02-09 14:35:04 -0500

    Create simulate.js

---
+++ simulate.js
@@ -0,0 +1,10 @@
+function simulatePPClient() {
+    
+    var hickoryFarmsFile = new DiffFile("www.hickoryfarms.com", "Privacy Policy.txt", "41d6f3ea68cf871152b433b13069125284e48b36");
+
+    PPClient.get_new_and_old_file(hickoryFarmsFile, function(file) {
+       
+        var diff = HtmlDiff.formatTextDiff(file.old, file.current);
+        $(#diff).append(diff);
+    });
+}
